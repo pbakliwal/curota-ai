149cb7bc92e75496c2c6cdaf2788e3e58d7a42bf
preetish.bakliwal@zensar.com <preetish.bakliwal@zensar.com>
2024-11-21 16:47:49 +0530

    second commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+document.getElementById("emailForm").addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name1 = document.getElementsByName("name").value
+    const email1 = document.getElementsByName("email").value;
+    const description = document.getElementsByName("message").value;
+
+});
+
+document.getElementById('emailForm').addEventListener('submit', function (event) {
+    event.preventDefault(); 
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const description = document.getElementById('message').value;     
+
+    var templateParams = {
+        from_name: name,
+        from_email: email,
+        message:description
+      };
+      
+      emailjs.send('service_8h52r8h', 'template_zpclgwf', templateParams).then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+          alert("Message sent");
+        },
+        (error) => {
+          console.log('FAILED...', error);
+        },
+      );
+});
